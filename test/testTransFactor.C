294bf22a2ced8d01fb20bebb9982c841dc833cb7
Will Buttinger <will@cern.ch>
2017-09-09 17:55:49 +0100

    update transFactor test

--- test/testTransFactor.C
+++ test/testTransFactor.C
@@ -6,30 +6,36 @@
   
   
   //add an alpha nuisance parameter
-  RooRealVar alpha("alpha","alpha",-5,5);
-  h_sr.AddParameter(alpha);
+  RooRealVar alpha("alpha","alpha",0,-5,5);
+  h_sr.addParameter(alpha);
   
-  alpha.setVal(1);
+  alpha = 1;
   h_sr.Fill(7); //SR value is 7 if alpha = 1
-  alpha.setVal(-1);
+  alpha = -1;
   h_sr.Fill(4); //SR value is 4 if alpha = -1
-  alpha.setVal(0);alpha.setError(1);
+  alpha = 0;
+  
+  //normally constrain it:
+  alpha.setStringAttribute("constraintType","normal");
   
   
   TRooH0D h_cr("h_cr","h_cr");
   h_cr.Fill(50);
-  h_cr.AddParameter(alpha);
-  alpha.setVal(1);h_cr.Fill(60);
-  alpha.setVal(-1);h_cr.Fill(40);
+  h_cr.addParameter(alpha);
   
-  auto tFactor = h_sr.createTransFactor( &h_cr );
+  alpha=1;h_cr.Fill(60);
+  alpha=-1;h_cr.Fill(40);
+  
+  auto tFactor = h_sr.createTransFactor( &h_cr ); //this makes sr = tFactor * cr_nom
+  tFactor->Print(); //will have value of 0.1 for alpha<=0, and varying above that (alpha 7/60 at alpha=1)
   
   RooRealVar mu("mu","mu",1,0,100);
-  h_cr.AddNormFactor(mu);
+  h_cr.addNormFactor(mu);
   
 
-  RooDataSet data("data","data",RooArgSet());
-  data.add(RooArgSet(),2);
+  h_sr.Draw("mu=1,alpha=0",""); //should be 5
+  h_sr.Draw("mu=1,alpha=-1",""); //still 5 (since value of cr_nom is used, alpha is ignored except in tFactor)
+  h_sr.Draw("mu=2,alpha=-1",""); //is 10 ... cr_nom gets multiplied by 2, and then transferred
   
 
 }
